19138702e62fd0ae89c419c6b33c0484902e6537
Trella <mr.trella@hotmail.com>
2026-04-21 13:12:45 +0200

    Fixade routing mellan home, convert och about och fixade kopplingarna där emelllan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Convert } from './convert/convert';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'convert', component: Convert },
+    { path: 'about', component: About }
+];
