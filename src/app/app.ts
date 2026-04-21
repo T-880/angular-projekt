b839cec998877186155686d89682f34f370c14bf
Trella <mr.trella@hotmail.com>
2026-04-21 14:58:49 +0200

    Lagt till FormsModule i app-komponenten och konverteringskomponenten för att stödja formulärhanteringen så att den fungerar. Uppdaterade konverterings- och header-komponenterna med nödvändiga imports och har nu fått navigationen att fungera.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/convert/convert.ts
+++ src/app/convert/convert.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.html',
   styleUrl: './convert.css',
 })
-export class Convert {}
+export class Convert {
+
+  meters: number = 0;
+  feet: number = 0;
+
+  celsius: number = 0;
+  fahrenheit: number = 0;
+
+  convertMeters() {
+    this.feet = this.meters * 3.28084;
+  }
+
+  convertCelsius() {
+    this.fahrenheit = (this.celsius * 9 / 5) + 32;
+  }
+}
