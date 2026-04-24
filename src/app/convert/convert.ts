d97a3cc05440c7d570ccce7d49dc86133247da49
Trella <mr.trella@hotmail.com>
2026-04-24 16:40:13 +0200

    Lagt till funktionalitet (som jag missat) för konvertering mellan feet och meter samt Celsius och Fahrenheit i konverteringskomponenten.

--- src/app/convert/convert.ts
+++ src/app/convert/convert.ts
@@ -12,14 +12,28 @@ export class Convert {
   meters: number = 0;
   feet: number = 0;
 
+  feetInput: number = 0;
+  metersResult: number = 0;
+
   celsius: number = 0;
   fahrenheit: number = 0;
+
+  fahrenheitInput: number = 0;
+  celsiusResult: number = 0;
 
   convertMeters() {
     this.feet = this.meters * 3.28084;
   }
 
+  convertFeet() {
+    this.metersResult = this.feetInput / 3.28084;
+  }
+
   convertCelsius() {
     this.fahrenheit = (this.celsius * 9 / 5) + 32;
+  }
+
+  convertFahrenheit() {
+    this.celsiusResult = (this.fahrenheitInput - 32) * 5 / 9;
   }
 }
